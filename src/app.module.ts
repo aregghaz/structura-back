31222dec8623c9f3ed89f62bc108c0c32228d4a2
Areg <aregghaz@gmail.com>
2023-12-19 13:33:05 +0400

    sd

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './migration/user.entry';
-import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
